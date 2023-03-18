705d9ef315cbbe30a160f8c88c17041dd36c2dba
AlexKoktysh <awot0058@gmail.com>
2023-03-18 12:36:47 +0300

    fixed

--- src/components/DatePicker/date-picker.js
+++ src/components/DatePicker/date-picker.js
@@ -26,7 +26,9 @@ function DatePickerControl(props) {
     const setError = () => {
         if (props.item.fieldName === "rights_date") {
             const el = document.getElementsByClassName("MuiFormLabel-root MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-sizeSmall MuiInputLabel-outlined MuiFormLabel-colorPrimary Mui-disabled Mui-error MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-sizeSmall MuiInputLabel-outlined css-1pysi21-MuiFormLabel-root-MuiInputLabel-root");
-            el[0].style.color = "rgba(0, 0, 0, 0.38)";
+            if (el && el[0] && el[0]?.style) {
+                el[0].style.color = "rgba(0, 0, 0, 0.38)";
+            }
         }
     };
     useEffect(() => {
